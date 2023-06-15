3099ca60da4fd22ad1f29b31506d12c0df1ccaf4
Arda Altunel <ardaltunelmain@gmail.com>
2023-06-15 04:01:10 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
-<? include '../cdn.ardaltunel.com/ardaltunel.php'; ?>
-<?= $Doctype ?><?= $Lang ?>
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
     <title>Translate - Arda Altunel</title>
-    <?= $Meta ?><?= $GoogleTag ?><?= $GoogleAdSanse ?><?= $MetaIcons ?>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="<?= $DomainUrlFullPath ?>/assets/css/tailwind.min.css"/>
-    <link rel="stylesheet" href="<?= $DomainUrlFullPath ?>/assets/css/style.css"/>
+    <link rel="stylesheet" href="assets/css/tailwind.min.css"/>
+    <link rel="stylesheet" href="assets/css/style.css"/>
 </head>
 <body class="bg-gray-900">
 
@@ -359,7 +361,7 @@
     </div>
 </section>
 
-<script src="<?= $DomainUrlFullPath ?>/assets/js/jquery.min.js"></script>
-<script src="<?= $DomainUrlFullPath ?>/assets/js/app.js"></script>
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/app.js"></script>
 </body>
 </html>
